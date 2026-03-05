1588acbb6522fc8982d71367e57ade5031f6c6f6
MatheusIzaquiel <matheusizaquiel1898@gmail.com>
2026-03-05 19:27:38 -0300

    chore: atualizando layout, estilos e configurações do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Category from "./Components/Category-Buttons/Category";
+
 import Hero from "./Components/Hero/Hero";
-import NavBar from "./Components/Navbar/NavBar";
+import Category from "./Components/Products/Category";
 import ProductsSection from "./Components/Products/ProductsSection";
 
 export default function Home() {
   return (
     <main className="w-full bg-[#0E0E0E]">
-      <NavBar />
       <Hero />
       <Category />
       <ProductsSection />
